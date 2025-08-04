d206fe997b2ec57c4f6d0c84e9da29591ea961d5
labib0x0hunter <labibfaisal450@gmail.com>
2025-08-04 14:30:59 +0600

    Refactor Basic's Codes

--- Basic/Intro/hello.cpp
+++ Basic/Intro/hello.cpp
@@ -3,20 +3,22 @@ using namespace std;
 
 #define endl '\n'
 
-void solution() { cout << "Hello World" << endl; }
+void solution() {
+    cout << "Hello World" << endl;
+}
 
 int32_t main() {
-  ios::sync_with_stdio(false);
-  cin.tie(0);
+    ios::sync_with_stdio(false);
+    cin.tie(0);
 
-  bool TEST_CASE = !true;
-  int t_c = 1;
-  if (TEST_CASE)
-    cin >> t_c;
+    bool TEST_CASE = !true;
+    int t_c = 1;
+    if (TEST_CASE)
+        cin >> t_c;
 
-  while (t_c--) {
-    solution();
-  }
+    while (t_c--) {
+        solution();
+    }
 
-  return 0;
+    return 0;
 }
